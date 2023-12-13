4478cf413ec256ca9145f398b109bd725f2690fa
jakobottar <jakobottar@pm.me>
2023-12-13 10:54:35 -0700

    add imagefolder creation function and fix image conversion bug

--- main.py
+++ main.py
@@ -4,20 +4,40 @@ import matplotlib.pyplot as plt
 
 import nfsdata
 
-nfsdata.build_nfs_dataset(
-    src_dir="/scratch_nvme/jakobj/all-morpho-images/",
-    dest_dir="/scratch_nvme/jakobj/nfs/nfs-something/",
+# from torchvision import transforms
+
+
+nfsdata.build_lmdb_datasets(
+    src_dir="/scratch_nvme/jakobj/nfs/all-morpho-images/",
+    dest_dir="/scratch_nvme/jakobj/nfs/lmdbs/full",
     config_file="./dataset_config.yml",
     num_threads=16,
 )
 
-dataset = nfsdata.NFSDataset("train", "/scratch_nvme/jakobj/nfs/nfs-something/")
+# nfsdata.build_imagefolder_datasets(
+#     src_dir="/scratch_nvme/jakobj/all-morpho-images/",
+#     dest_dir="/usr/sci/scratch/jakobj/nfs/tiles/128",
+#     config_file="./dataset_config.yml",
+#     num_threads=16,
+# )
 
-# make a grid of 4 sample images
-fig, axs = plt.subplots(2, 2)
-for i in range(2):
-    for j in range(2):
-        axs[i, j].imshow(dataset[random.randint(0, len(dataset))][0].permute(1, 2, 0))
+dataset = nfsdata.NFSDataset(
+    "train",
+    "/scratch_nvme/jakobj/nfs/lmdbs/full",
+)
+
+# dataset = nfsdata.ImageFolderDataset(
+#     "train",
+#     "/usr/sci/scratch/jakobj/nfs/tiles/128",
+#     transform=transforms.ToTensor(),
+# )
+
+# make a grid of sample images
+fig, axs = plt.subplots(4, 4)
+for i in range(4):
+    for j in range(4):
+        img = dataset[random.randint(0, len(dataset))][0].permute(1, 2, 0)
+        axs[i, j].imshow(img)
         axs[i, j].axis("off")
 
 fig.savefig("sample.png")
--- nfsdata/dataset.py
+++ nfsdata/dataset.py
@@ -10,17 +10,24 @@ from glob import glob
 from multiprocessing import Pool
 
 import lmdb
+import pandas as pd
 import pyxis as px
 import torch
 import yaml
 from pandas import DataFrame
+from PIL import Image
 
-from .utils import filter_dataframe, get_metadata, make_lmdb, print_green
+from .utils import (
+    filter_dataframe,
+    get_metadata,
+    make_imagefolder,
+    make_lmdb,
+    print_green,
+)
 
 
-def build_nfs_dataset(
+def build_nfs_datasets(
     src_dir: str,
-    dest_dir: str,
     config_file: str,
     num_threads: int = 4,
 ) -> None:
@@ -64,6 +71,7 @@ def build_nfs_dataset(
     # TODO: customizable label column
     train_val["Label"] = train_val["StartingMaterial"] + train_val["Material"]
     print(train_val["Label"].astype("category").cat.categories)
+    train_val["LabelStr"] = train_val["Label"].astype("category")
     train_val["Label"] = train_val["Label"].astype("category").cat.codes
     # TODO: provide a back-conversion to original labels
 
@@ -93,6 +101,14 @@ def build_nfs_dataset(
         )
     print_green("done.")
 
+    return datasets
+
+
+def build_lmdb_datasets(
+    src_dir: str, dest_dir: str, config_file: str, num_threads: int = 4
+) -> None:
+    datasets = build_nfs_datasets(src_dir, config_file, num_threads)
+
     # package datasets into lmdbs
     for dataset in datasets:
         print(f"Building {dataset['name']} dataset... ", end="\n", flush=True)
@@ -108,6 +124,32 @@ def build_nfs_dataset(
                 print(
                     f"    '{key}' <- dtype: {sample[key].dtype}, shape: {sample[key].shape}"
                 )
+
+    print("datasets built!")
+
+
+def build_imagefolder_datasets(
+    src_dir: str, dest_dir: str, config_file: str, num_threads: int = 4
+) -> None:
+    datasets = build_nfs_datasets(src_dir, config_file, num_threads)
+
+    # create image folders with data and labels
+    for dataset in datasets:
+        print(f"Building {dataset['name']} dataset... ", end="\n", flush=True)
+        make_imagefolder(
+            dest_dir, dataset["name"], dataset["dataframe"], dataset["type"]
+        )
+        print_green("done.")
+
+    # verify datasets
+    # for dataset in glob(os.path.join(dest_dir, "*")):
+    #     with px.Reader(dirpath=dataset) as db:
+    #         print(f"{dataset}:\n    number of samples: {len(db)}")
+    #         sample = db.get_sample(17)
+    #         for key in db.get_data_keys():
+    #             print(
+    #                 f"    '{key}' <- dtype: {sample[key].dtype}, shape: {sample[key].shape}"
+    #             )
 
     print("datasets built!")
 
@@ -136,7 +178,7 @@ class NFSDataset(torch.utils.data.Dataset):
             ) from exc
 
     def __len__(self):
-        return len(self.root_dir)
+        return len(self.db)
 
     def __getitem__(self, key):
         data = self.db[key]
@@ -158,3 +200,44 @@ class NFSDataset(torch.utils.data.Dataset):
             return str(self.db) + "\n" + format_string
         else:
             return str(self.db)
+
+
+class ImageFolderDataset(torch.utils.data.Dataset):
+    """
+    Nuclear Forensics dataset.
+
+    Args:
+        split (string): Dataset split to load, one of "train", "val", or ood splits defined in dataset_config.yml
+        root_dir (string): Root directory of built dataset
+        transform (callable, optional): Optional transform to be applied to a sample
+    """
+
+    def __init__(self, split: str, root_dir: str, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.dirpath = os.path.join(self.root_dir, split)
+
+        # load dataset metadata file
+        try:
+            self.df = pd.read_csv(os.path.join(self.dirpath, "metadata.csv"))
+        except lmdb.Error as exc:
+            raise FileNotFoundError(
+                f"Dataset {self.dirpath} does not exist, make sure it has been built."
+            ) from exc
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, key):
+        data = self.df.iloc[key].to_dict()
+        data["image"] = Image.open(
+            os.path.join(self.dirpath, data["filename"])
+        ).convert("RGB")
+
+        if self.transform:
+            data["image"] = self.transform(data["image"])
+
+        return data["image"], data["label"]
+
+    def __repr__(self):
+        return "ImageFolderDataset"

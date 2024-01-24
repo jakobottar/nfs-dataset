aac0a4ef6af2bff154f3e99fda7f3e37b5e682f7
jakobottar <jakobottar@pm.me>
2024-01-24 11:12:37 -0700

    update

--- main.py
+++ main.py
@@ -4,33 +4,17 @@ import matplotlib.pyplot as plt
 
 import nfsdata
 
-# from torchvision import transforms
-
-
 nfsdata.build_lmdb_datasets(
-    src_dir="/scratch_nvme/jakobj/nfs/all-morpho-images/",
-    dest_dir="/scratch_nvme/jakobj/nfs/lmdbs/full",
+    src_dir="/usr/sci/projs/DeepLearning/Jakob_Dataset/box/all-morpho-images/",
+    dest_dir="/scratch/jakobj/nfs/lmdbs/256",
     config_file="./dataset_config.yml",
     num_threads=16,
 )
 
-# nfsdata.build_imagefolder_datasets(
-#     src_dir="/scratch_nvme/jakobj/all-morpho-images/",
-#     dest_dir="/usr/sci/scratch/jakobj/nfs/tiles/128",
-#     config_file="./dataset_config.yml",
-#     num_threads=16,
-# )
-
 dataset = nfsdata.NFSDataset(
     "train",
-    "/scratch_nvme/jakobj/nfs/lmdbs/full",
+    "/scratch/jakobj/nfs/lmdbs/256",
 )
-
-# dataset = nfsdata.ImageFolderDataset(
-#     "train",
-#     "/usr/sci/scratch/jakobj/nfs/tiles/128",
-#     transform=transforms.ToTensor(),
-# )
 
 # make a grid of sample images
 fig, axs = plt.subplots(4, 4)
